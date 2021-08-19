7f9c4228eea682f7e7123658db6de15b97452a50
yb8350 <yb8350@naver.com>
2021-08-19 22:34:46 +0900

    Updates

---
+++ static/js/main.73a0efe7.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpbeverage_psychological_test=this.webpackJsonpbeverage_psychological_test||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),s=c(3),o=c.n(s),i=c(0);var a=function(){return Object(i.jsx)("div",{children:"\uc548\ub155"})};o.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(a,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.73a0efe7.chunk.js.map
